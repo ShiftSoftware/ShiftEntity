f402eb0d54300f1f209b05531ef61dd59dfd38dc
Nharo <Nahropro@gmail.com>
2022-11-22 11:26:17 +0300

    Create ShiftDbContext

---
+++ ShiftEntity/ShiftDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShiftSoftware.ShiftEntity.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftSoftware.ShiftEntity.Core;
+
+public abstract class ShiftDbContext : DbContext
+{
+    public ShiftDbContext() : base()
+    {
+    }
+
+    public ShiftDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ConfigureShiftEntity();
+    }
+}
