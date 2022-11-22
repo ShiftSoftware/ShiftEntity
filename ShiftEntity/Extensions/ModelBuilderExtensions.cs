6a177c3a8b9ef5353862301b0db82251714f36c0
Nharo <Nahropro@gmail.com>
2022-11-22 11:26:00 +0300

    Create ConfigureShiftEntity extension method to ModelBuilder

---
+++ ShiftEntity/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftSoftware.ShiftEntity.Core.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static ModelBuilder ConfigureShiftEntity(this ModelBuilder modelBuilder)
+    {
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            var clrType = entityType.ClrType;
+
+            if (clrType.IsAssignableTo(typeof(IShiftEntity)))
+            {
+                var isTemporal = clrType.GetCustomAttributes(true).LastOrDefault(x => (x as TemporalShiftEntity) != null);
+
+                if (isTemporal != null)
+                {
+                    modelBuilder.Entity(entityType.ClrType).ToTable(b => b.IsTemporal());
+                }
+            }
+        }
+
+        return modelBuilder;
+    }
+}
