42484775975bfe670fbef492ad560e5ed8010a33
Aza Asim <azaasim@gmail.com>
2022-11-29 15:22:41 +0300

    Remove ICurdDTO

--- ShiftEntity/IShiftEntityCreate.cs
+++ ShiftEntity/IShiftEntityCreate.cs
@@ -4,7 +4,6 @@ namespace ShiftSoftware.ShiftEntity.Core;
 
 public interface IShiftEntityCreate<EntityType,CreateDTOType>
     where EntityType : class
-    where CreateDTOType : ICrudDTO
 {
     public EntityType Create(CreateDTOType crudDto, Guid? userId = null);
 }
--- ShiftEntity/IShiftEntityUpdate.cs
+++ ShiftEntity/IShiftEntityUpdate.cs
@@ -4,7 +4,6 @@ namespace ShiftSoftware.ShiftEntity.Core
 {
     public interface IShiftEntityUpdate<EntityType,UpdateDTOType>
         where EntityType: class
-        where UpdateDTOType : ICrudDTO
     {
         public EntityType Update(UpdateDTOType crudDto, Guid? userId = null);
     }
