016a786722e24eb0735449d79244654a802438e5
Aza Asim <azaasim@gmail.com>
2023-01-11 05:05:14 +0300

    ShiftEntityMixedDTO

---
+++ ShiftEntity/Dtos/ShiftEntityMixedDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+
+namespace ShiftSoftware.ShiftEntity.Core.Dtos
+{
+    public class ShiftEntityMixedDTO: ShiftEntityDTOBase
+    {
+        [DataType(DataType.DateTime)]
+        public DateTime CreateDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime LastSaveDate { get; set; }
+
+        public Guid? CreatedByUserID { get; set; }
+
+        public Guid? LastSavedByUserID { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public ICollection<RevisionDTO>? Revisions { get; set; }
+    }
+}
