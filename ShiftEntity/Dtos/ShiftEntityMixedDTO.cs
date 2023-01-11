8e7053355ca8bc5cece21e035dc7a1a51c13e3a0
Aza Asim <azaasim@gmail.com>
2023-01-11 05:33:27 +0300

    ShiftEntityMixedDTO inherit from ShiftEntityDTO

--- ShiftEntity/Dtos/ShiftEntityMixedDTO.cs
+++ ShiftEntity/Dtos/ShiftEntityMixedDTO.cs
@@ -4,20 +4,8 @@ using System.Collections.Generic;
 
 namespace ShiftSoftware.ShiftEntity.Core.Dtos
 {
-    public class ShiftEntityMixedDTO: ShiftEntityDTOBase
+    public class ShiftEntityMixedDTO: ShiftEntityDTO
     {
-        [DataType(DataType.DateTime)]
-        public DateTime CreateDate { get; set; }
-
-        [DataType(DataType.DateTime)]
-        public DateTime LastSaveDate { get; set; }
-
-        public Guid? CreatedByUserID { get; set; }
-
-        public Guid? LastSavedByUserID { get; set; }
-
-        public bool IsDeleted { get; set; }
-
         public ICollection<RevisionDTO>? Revisions { get; set; }
     }
 }
