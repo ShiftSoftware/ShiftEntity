8110da19eddcc48c41d81ed941ca9316015df4a2
Nharo <Nahropro@gmail.com>
2023-01-23 10:13:38 +0300

    ODataDTO

---
+++ ShiftEntity/Dtos/ODataDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftSoftware.ShiftEntity.Core.Dtos;
+
+public class ODataDTO<TValue>
+{
+    public Int64 Count { get; set; }
+    public TValue Value { get; set; }
+}
