7765b98d97ebc2b230eaff7fce16b422311e0d91
Nahro <nahropro@gmail.com>
2023-01-23 18:32:12 +0300

    ODataDTO count nullable and fix mapping count property

--- ShiftEntity/Dtos/ODataDTO.cs
+++ ShiftEntity/Dtos/ODataDTO.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ShiftSoftware.ShiftEntity.Core.Dtos;
 
 public class ODataDTO<TValue>
 {
-    public Int64 Count { get; set; }
+    [JsonPropertyName("@odata.count")]
+    public Int64? Count { get; set; }
     public TValue Value { get; set; }
 }
