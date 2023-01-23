c9fc7196ca6bc116e75a1532273743daa6dd4377
Nharo <Nahropro@gmail.com>
2023-01-23 09:57:46 +0300

    ShiftEntityException make http-status-code optional

--- ShiftEntity/ShiftEntityException.cs
+++ ShiftEntity/ShiftEntityException.cs
@@ -1,30 +1,35 @@
 ﻿using System.Net;
 
-namespace ShiftSoftware.ShiftEntity.Core
+namespace ShiftSoftware.ShiftEntity.Core;
+
+public class ShiftEntityException : System.Exception
 {
-    public class ShiftEntityException : System.Exception
-    {
-        public Message Message { get; set; }
-        public int HttpStatusCode { get; set; }
+    public Message Message { get; set; }
+    public int HttpStatusCode { get; set; }
 
-        public ShiftEntityException() {
-            HttpStatusCode= 400;
-        }
-
-        public ShiftEntityException(
-            string messageTitle,
-            string messageBody,
-            int httpStatusCode = 400)
-        {
-            this.Message = new Message { Title = messageTitle, Body = messageBody };
-            this.HttpStatusCode = httpStatusCode;
-        }
-
-        public ShiftEntityException(
-            string messageTitle,
-            string messageBody,
-            HttpStatusCode httpStatusCode = System.Net.HttpStatusCode.BadRequest) :
-            this(messageTitle, messageBody, (int) httpStatusCode)
-        { }
+    public ShiftEntityException() {
+        HttpStatusCode = (int)System.Net.HttpStatusCode.BadRequest;
     }
+
+    public ShiftEntityException(
+        string messageTitle,
+        string messageBody,
+        int httpStatusCode)
+    {
+        this.Message = new Message { Title = messageTitle, Body = messageBody };
+        this.HttpStatusCode = httpStatusCode;
+    }
+
+    public ShiftEntityException(
+        string messageTitle,
+        string messageBody,
+        HttpStatusCode httpStatusCode) :
+        this(messageTitle, messageBody, (int) httpStatusCode)
+    { }
+
+    public ShiftEntityException(
+        string messageTitle,
+        string messageBody) :
+        this(messageTitle, messageBody, System.Net.HttpStatusCode.BadRequest)
+    { }
 }
