23c93336c28196119f4ec5a02336d50779bcb84b
Nahro <nahropro@gmail.com>
2023-03-03 03:41:22 +0300

    Add 'HttpResponse' class

---
+++ ShiftEntity/HttpResponse.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+namespace ShiftSoftware.ShiftEntity.Core;
+
+public class HttpResponse<T>
+{
+    public HttpStatusCode StatusCode { get; set; }
+
+    public string ErrorMessage { get; set; }
+
+    public T? Data { get; set; }
+
+    public bool IsSuccess
+    {
+        get
+        {
+            return (int)StatusCode >= 200 && (int)StatusCode <= 299;
+        }
+    }
+
+    public HttpResponse(T data, HttpStatusCode statusCode)
+    {
+        StatusCode = statusCode;
+        Data = data;
+    }
+
+    public HttpResponse(string errorMessage, HttpStatusCode statusCode)
+    {
+        ErrorMessage = errorMessage;
+        StatusCode = statusCode;
+    }
+}
