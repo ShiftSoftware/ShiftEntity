d6482fe16a63caac69e1e0738e61e9ce2e37a029
Aza Asim <azaasim@gmail.com>
2023-03-28 16:08:24 +0300

    Enum Description Extension Method

---
+++ ShiftEntity.Model/Extensions/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace ShiftSoftware.ShiftEntity.Model.Extensions;
+
+public static class EnumExtensions
+{
+    public static string Describe(this Enum val)
+    {
+        return GetDescriptionString(val);
+    }
+
+    private static string GetDescriptionString<T>(T val)
+    {
+        var attributes = (DescriptionAttribute[])val!
+           .GetType()
+           .GetField(val.ToString()!)!
+           .GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+        return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+    }
+}
