811bcb3bc8e1ca7b8749452ac69a59b5af0c7a30
Aza Asim <azaasim@gmail.com>
2023-08-29 19:08:41 +0300

    Add Identity Claim Constants

---
+++ ShiftEntity.Core/Constants.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ShiftSoftware.ShiftEntity.Core;
+
+public class Constants
+{
+    public const string RegionIdClaim = "ShiftSoftware/ShiftEntity/Claims/RegionId";
+    public const string CompanyIdClaim = "ShiftSoftware/ShiftEntity/Claims/CompanyId";
+    public const string CompanyBranchIdClaim = "ShiftSoftware/ShiftEntity/Claims/CompanyBranchId";
+}
