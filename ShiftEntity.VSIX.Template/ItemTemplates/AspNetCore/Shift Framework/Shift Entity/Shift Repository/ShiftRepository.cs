d0224c80971a840f26215b67693615bb2269a640
Sarmad Ahmed <sarmadkoye@hotmail.com>
2023-09-24 14:48:34 +0300

    #85zu0fwf9: Fix Shift Repository Constructor

--- ShiftEntity.VSIX.Template/ItemTemplates/AspNetCore/Shift Framework/Shift Entity/Shift Repository/ShiftRepository.cs
+++ ShiftEntity.VSIX.Template/ItemTemplates/AspNetCore/Shift Framework/Shift Entity/Shift Repository/ShiftRepository.cs
@@ -1,13 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
-using ShiftSoftware.EFCore.SqlServer;
+﻿using AutoMapper;
+using ShiftSoftware.ShiftEntity.Core;
+using ShiftSoftware.ShiftEntity.EFCore;
 
 namespace $rootnamespace$
 {
     public class $fileinputname$Repository : 
-    ShiftRepository<DB, $fileinputname$, $fileinputname$ListDTO, $fileinputname$DTO>,
+    ShiftRepository<DB, $fileinputname$, $fileinputname$ListDTO, $fileinputname$DTO, $fileinputname$DTO>,
     IShiftRepositoryAsync<$fileinputname$, $fileinputname$ListDTO, $fileinputname$DTO>
     {
-       public $fileinputname$Repository()
+       public $fileinputname$Repository(DB dB, IMapper mapper) : base(dB, dB.$fileinputname$s, mapper)
        {
        }
     }
