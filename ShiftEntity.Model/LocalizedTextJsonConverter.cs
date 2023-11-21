ba0b0e98a2a517cf777e15cae5bb1d04acae4f99
Aza Asim <azaasim@gmail.com>
2023-11-21 22:35:38 +0300

    Add LocalizedTextJsonConverter

---
+++ ShiftEntity.Model/LocalizedTextJsonConverter.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ShiftSoftware.ShiftEntity.Model;
+public class LocalizedTextJsonConverter : JsonConverter<string>
+{
+    public static string UserLanguage = "en";
+
+    public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.String)
+        {
+            var jsonString = reader.GetString() ?? string.Empty;
+
+            try
+            {
+                var json = JsonDocument.Parse(jsonString);
+
+                var localizedText = json.Deserialize<Dictionary<string, string>>()!;
+
+                if (localizedText.TryGetValue(UserLanguage, out var localizedValue))
+                {
+                    return localizedValue;
+                }
+
+                if (localizedText.TryGetValue("en", out var defaultValue))
+                {
+                    return defaultValue;
+                }
+            }
+            catch
+            {
+                return jsonString;
+            }
+        }
+
+        return string.Empty;
+    }
+
+    public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+}
