1f9cf885b3ae3b4a200e5f54fe875fabbbb5eee3
Nahro Fuad <nahropro@gmail.com>
2024-02-12 21:52:58 +0300

    Add User-Group

---
+++ ShiftEntity.Model/HashIds/UserGroupHashIdConverter.cs
@@ -0,0 +1,8 @@
+﻿namespace ShiftSoftware.ShiftEntity.Model.HashIds;
+
+public class UserGroupHashIdConverter : JsonHashIdConverterAttribute<UserGroupHashIdConverter>
+{
+    public UserGroupHashIdConverter() : base(HashId.IdentityHashIdMinLength, HashId.IdentityHashIdSalt, HashId.IdentityHashIdAlphabet, true)
+    {
+    }
+}
