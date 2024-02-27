ab3fe6274d7180e2fddb33aa18ec30eb1fc426ca
Nahro Fuad <nahropro@gmail.com>
2024-02-27 23:15:00 +0300

    Mode 'ID' in identity replication models to base models and ignore it by Text.Json

--- ShiftEntity.Model/Replication/ReplicationModel.cs
+++ ShiftEntity.Model/Replication/ReplicationModel.cs
@@ -10,4 +10,7 @@ public abstract class ReplicationModel : ShiftEntityViewAndUpsertDTO
     [System.Text.Json.Serialization.JsonPropertyName(nameof(id))]
     [Newtonsoft.Json.JsonProperty(nameof(id))]
     public string id { get; set; }
+
+    [System.Text.Json.Serialization.JsonIgnore]
+    override public string? ID { get { return id; } set { id = value ?? ""; } }
 }
