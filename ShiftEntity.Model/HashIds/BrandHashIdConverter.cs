8223b11a996d95c5f188aef2d14d9924b4d92029
Nahro Fuad <nahropro@gmail.com>
2024-03-19 17:01:27 +0300

    Add Brand

---
+++ ShiftEntity.Model/HashIds/BrandHashIdConverter.cs
@@ -0,0 +1,8 @@
+﻿namespace ShiftSoftware.ShiftEntity.Model.HashIds;
+
+public class BrandHashIdConverter : JsonHashIdConverterAttribute<BrandHashIdConverter>
+{
+    public BrandHashIdConverter() : base(HashId.IdentityHashIdMinLength, HashId.IdentityHashIdSalt, HashId.IdentityHashIdAlphabet, true)
+    {
+    }
+}
