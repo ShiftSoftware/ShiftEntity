49ad4e9708ab3ec98420be26b1ea28fc21e10a3f
Aza Asim <aza@shift.software>
2024-04-12 02:29:52 +0300

    #86ep35hda: New Platform: Standard Draft Flag

---
+++ ShiftEntity.Core/Flags/IHasDraft.cs
@@ -0,0 +1,22 @@
+﻿
+using ShiftSoftware.ShiftEntity.Model.Dtos;
+
+namespace ShiftSoftware.ShiftEntity.Core.Flags;
+
+public interface IEntityHasDraft<Entity>
+    where Entity : ShiftEntityBase, new()
+{
+    bool IsDraft { get; set; }
+}
+
+public interface IHasDraftCheckBox<ViewAndUpsertDTO>
+    where ViewAndUpsertDTO : ShiftEntityViewAndUpsertDTO, new()
+{
+    bool? IsDraft { get; set; }
+}
+
+public interface IHasDraftColumn<ListDTO>
+    where ListDTO : ShiftEntityDTOBase, new()
+{
+    bool IsDraft { get; set; }
+}
