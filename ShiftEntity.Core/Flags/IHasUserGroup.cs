e60a64581211b2991c1cde9119d4bc7cd43f3059
Aza Asim <aza@shift.software>
2024-04-22 04:16:56 +0300

    Add IHasUserGroup Flag

---
+++ ShiftEntity.Core/Flags/IHasUserGroup.cs
@@ -0,0 +1,7 @@
+﻿namespace ShiftSoftware.ShiftEntity.Core.Flags;
+
+public interface IEntityHasUserGroup<Entity>
+    where Entity : ShiftEntityBase, new()
+{
+    long? UserGroupID { get; set; }
+}
