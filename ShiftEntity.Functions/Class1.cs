963e7039625a8be6828508ca9e769f3e1b325ad1
Nahro Fuad <nahropro@gmail.com>
2024-05-28 20:36:42 +0300

    Add ShiftEntity.Functions project

---
+++ ShiftEntity.Functions/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftSoftware.ShiftEntity.Functions
+{
+    internal class Class1
+    {
+    }
+}
