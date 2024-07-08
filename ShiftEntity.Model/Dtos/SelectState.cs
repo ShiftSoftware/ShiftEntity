de5ce4b162532e6783e8afc74b5e383f30a67600
Saman <gtsaman@gmail.com>
2024-07-08 23:20:05 +0300

    Add SelectState to DTOs

---
+++ ShiftEntity.Model/Dtos/SelectState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShiftSoftware.ShiftEntity.Model.Dtos;
+
+public class SelectState<T>
+{
+    public bool All { get; set; }
+    public List<T> Items { get; set; } = [];
+    public int Count => All ? Total : Items.Count;
+    public int Total { get; set; }
+    public string? Filter { get; set; }
+
+    public void Clear()
+    {
+        Items.Clear();
+        All = false;
+        Filter = null;
+    }
+}
