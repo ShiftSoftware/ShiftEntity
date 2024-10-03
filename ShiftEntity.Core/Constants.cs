ce94d711bf1fa030db55f1099955dc139bb06d21
Nahro Fuad <nahropro@gmail.com>
2024-10-03 09:18:16 +0300

    Add country

--- ShiftEntity.Core/Constants.cs
+++ ShiftEntity.Core/Constants.cs
@@ -3,6 +3,7 @@ namespace ShiftSoftware.ShiftEntity.Core;
 
 public class Constants
 {
+    public const string CountryIdClaim = "ShiftSoftware/ShiftEntity/Claims/CountryId";
     public const string RegionIdClaim = "ShiftSoftware/ShiftEntity/Claims/RegionId";
     public const string CompanyIdClaim = "ShiftSoftware/ShiftEntity/Claims/CompanyId";
     public const string CompanyBranchIdClaim = "ShiftSoftware/ShiftEntity/Claims/CompanyBranchId";
