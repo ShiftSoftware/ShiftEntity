07c1f8850922af95968bef105b06ef2e49a7a1e9
Aza Asim <aza@shift.software>
2025-01-08 16:09:13 +0300

    Unique Hash Should Exclude Deleted Entites

--- ShiftEntity.EFCore/Extensions/ModelBuilderExtensions.cs
+++ ShiftEntity.EFCore/Extensions/ModelBuilderExtensions.cs
@@ -42,7 +42,7 @@ public static class ModelBuilderExtensions
                     .Entity(clrType)
                     .HasIndex(uniqueHashColumnName)
                     .IsUnique()
-                    .HasFilter($"{uniqueHashColumnName} IS NOT NULL");
+                    .HasFilter($"{uniqueHashColumnName} IS NOT NULL and IsDeleted = 0");
             }
         }
 
