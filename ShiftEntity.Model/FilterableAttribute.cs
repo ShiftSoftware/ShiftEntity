43de9a06bebb8f2906708ebfbf543a80bde85dfa
Saman-00 <gtsaman@gmail.com>
2025-03-19 16:58:53 +0300

    Add Filterable attribute and use it to enable or disable Filter on fields

---
+++ ShiftEntity.Model/FilterableAttribute.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ShiftSoftware.ShiftEntity.Model;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Class)]
+public class FilterableAttribute : Attribute
+{
+    public bool Immediate { get; private set; }
+    public bool Disabled { get; private set; }
+
+    public FilterableAttribute(bool immediate = false, bool disable = false)
+    {
+        Immediate = immediate;
+        Disabled = disable;
+    }
+}
