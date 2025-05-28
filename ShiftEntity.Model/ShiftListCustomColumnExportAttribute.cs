b046806b437f02baa46be68faa21facffe1effb2
AzaAsim <aza@shift.software>
2025-05-28 16:47:30 +0300

    Change Enum Handling in the New Grid Export & Move Attributes to ShiftEntity.Model

---
+++ ShiftEntity.Model/ShiftListCustomColumnExportAttribute.cs
@@ -0,0 +1,39 @@
+﻿namespace ShiftSoftware.ShiftEntity.Model;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+public class ShiftListCustomColumnExportAttribute : Attribute
+{
+    public string[] Parts { get; }
+
+    public ShiftListCustomColumnExportAttribute(params string[] parts)
+    {
+        Parts = parts;
+    }
+
+    public List<object> ToList()
+    {
+        var result = new List<object>();
+
+        foreach (var part in Parts)
+        {
+            if (part.StartsWith("Property."))
+            {
+                result.Add(new
+                {
+                    type = "property",
+                    value = part.Substring("Property.".Length)
+                });
+            }
+            else
+            {
+                result.Add(new
+                {
+                    type = "string",
+                    value = part
+                });
+            }
+        }
+
+        return result;
+    }
+}
---
+++ ShiftEntity.Model/ShiftListNumberFormatterExportAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace ShiftSoftware.ShiftEntity.Model;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+public class ShiftListNumberFormatterExportAttribute : Attribute
+{
+    public string Format { get; }
+
+    public ShiftListNumberFormatterExportAttribute(string format)
+    {
+        Format = format;
+    }
+}
