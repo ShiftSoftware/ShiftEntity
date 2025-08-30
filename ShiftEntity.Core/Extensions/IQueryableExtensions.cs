20aaa4f53380f4a397a3812d22526e26f6bc0d24
AzaAsim <aza@shift.software>
2025-08-30 10:32:38 +0300

    Default Data Level & Soft Delete Filter: Add IQueryable Extension Methods

---
+++ ShiftEntity.Core/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,65 @@
+﻿using ShiftSoftware.ShiftEntity.Core;
+using ShiftSoftware.ShiftEntity.Model.Dtos;
+using ShiftSoftware.ShiftEntity.Model.Flags;
+using System.Collections.Generic;
+
+namespace System.Linq;
+
+public static class IQueryableExtensions
+{
+    public static IQueryable<EntityType> ApplyDefaultCountryFilter<EntityType>(
+        this IQueryable<EntityType> query,
+        IDefaultDataLevelAccess defaultDataLevelAccess
+    ) where EntityType : IEntityHasCountry<EntityType>
+    {
+        return defaultDataLevelAccess.ApplyDefaultCountryFilter(query);
+    }
+
+    public static IQueryable<EntityType> ApplyDefaultRegionFilter<EntityType>(
+        this IQueryable<EntityType> query,
+        IDefaultDataLevelAccess defaultDataLevelAccess
+    ) where EntityType : IEntityHasRegion<EntityType>
+    {
+        return defaultDataLevelAccess.ApplyDefaultRegionFilter(query);
+    }
+
+    public static IQueryable<EntityType> ApplyDefaultCompanyFilter<EntityType>(
+        this IQueryable<EntityType> query,
+        IDefaultDataLevelAccess defaultDataLevelAccess
+    ) where EntityType : IEntityHasCompany<EntityType>
+    {
+        return defaultDataLevelAccess.ApplyDefaultCompanyFilter(query);
+    }
+
+    public static IQueryable<EntityType> ApplyDefaultBranchFilter<EntityType>(
+        this IQueryable<EntityType> query,
+        IDefaultDataLevelAccess defaultDataLevelAccess
+    ) where EntityType : IEntityHasCompanyBranch<EntityType>
+    {
+        return defaultDataLevelAccess.ApplyDefaultBranchFilter(query);
+    }
+
+    public static IQueryable<EntityType> ApplyDefaultBrandFilter<EntityType>(
+        this IQueryable<EntityType> query,
+        IDefaultDataLevelAccess defaultDataLevelAccess
+    ) where EntityType : IEntityHasBrand<EntityType>
+    {
+        return defaultDataLevelAccess.ApplyDefaultBrandFilter(query);
+    }
+
+    public static IQueryable<EntityType> ApplyDefaultCityFilter<EntityType>(
+        this IQueryable<EntityType> query,
+        IDefaultDataLevelAccess defaultDataLevelAccess
+    ) where EntityType : IEntityHasCity<EntityType>
+    {
+        return defaultDataLevelAccess.ApplyDefaultCityFilter(query);
+    }
+
+    public static IQueryable<EntityType> ApplyDefaultTeamFilter<EntityType>(
+        this IQueryable<EntityType> query,
+        IDefaultDataLevelAccess defaultDataLevelAccess
+    ) where EntityType : IEntityHasTeam<EntityType>
+    {
+        return defaultDataLevelAccess.ApplyDefaultTeamFilter(query);
+    }
+}
---
+++ ShiftEntity.Web/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.OData.Query;
+using ShiftSoftware.ShiftEntity.Model.Dtos;
+using ShiftSoftware.ShiftEntity.Web.Services;
+
+namespace System.Linq;
+
+public static class IQueryableExtensions
+{
+    public static IQueryable<EntityType> ApplyDefaultSoftDeleteFilter<EntityType>(
+        this IQueryable<EntityType> query,
+        ODataQueryOptions<EntityType> oDataQueryOptions
+    ) where EntityType : ShiftEntityDTOBase
+    {
+        return ODataIqueryable.ApplyDefaultSoftDeleteFilter(query, oDataQueryOptions);
+    }
+}
