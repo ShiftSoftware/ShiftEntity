ed946ae095255a1d7124122af6b37edb78b7900e
AzaAsim <aza@shift.software>
2025-08-30 10:12:55 +0300

    IDefaultDataLevelAccess: Rename Apply Filter methods

--- ShiftEntity.Core/IDefaultDataLevelAccess.cs
+++ ShiftEntity.Core/IDefaultDataLevelAccess.cs
@@ -15,13 +15,13 @@ public interface IDefaultDataLevelAccess
     public List<long?>? GetAccessibleCities();
     public List<long?>? GetAccessibleTeams();
 
-    public IQueryable<EntityType> ApplyDefaultFilterOnCountries<EntityType>(IQueryable<EntityType> query) where EntityType : IEntityHasCountry<EntityType>;
-    public IQueryable<EntityType> ApplyDefaultFilterOnRegions<EntityType>(IQueryable<EntityType> query) where EntityType : IEntityHasRegion<EntityType>;
-    public IQueryable<EntityType> ApplyDefaultFilterOnCompanies<EntityType>(IQueryable<EntityType> query) where EntityType : IEntityHasCompany<EntityType>;
-    public IQueryable<EntityType> ApplyDefaultFilterOnBranches<EntityType>(IQueryable<EntityType> query) where EntityType : IEntityHasCompanyBranch<EntityType>;
-    public IQueryable<EntityType> ApplyDefaultFilterOnBrands<EntityType>(IQueryable<EntityType> query) where EntityType : IEntityHasBrand<EntityType>;
-    public IQueryable<EntityType> ApplyDefaultFilterOnCities<EntityType>(IQueryable<EntityType> query) where EntityType : IEntityHasCity<EntityType>;
-    public IQueryable<EntityType> ApplyDefaultFilterOnTeams<EntityType>(IQueryable<EntityType> query) where EntityType : IEntityHasTeam<EntityType>;
+    public IQueryable<EntityType> ApplyDefaultCountryFilter<EntityType>(IQueryable<EntityType> query) where EntityType : IEntityHasCountry<EntityType>;
+    public IQueryable<EntityType> ApplyDefaultRegionFilter<EntityType>(IQueryable<EntityType> query) where EntityType : IEntityHasRegion<EntityType>;
+    public IQueryable<EntityType> ApplyDefaultCompanyFilter<EntityType>(IQueryable<EntityType> query) where EntityType : IEntityHasCompany<EntityType>;
+    public IQueryable<EntityType> ApplyDefaultBranchFilter<EntityType>(IQueryable<EntityType> query) where EntityType : IEntityHasCompanyBranch<EntityType>;
+    public IQueryable<EntityType> ApplyDefaultBrandFilter<EntityType>(IQueryable<EntityType> query) where EntityType : IEntityHasBrand<EntityType>;
+    public IQueryable<EntityType> ApplyDefaultCityFilter<EntityType>(IQueryable<EntityType> query) where EntityType : IEntityHasCity<EntityType>;
+    public IQueryable<EntityType> ApplyDefaultTeamFilter<EntityType>(IQueryable<EntityType> query) where EntityType : IEntityHasTeam<EntityType>;
 
 
     public bool HasDefaultDataLevelAccess<EntityType>(DefaultDataLevelAccessOptions defaultDataLevelAccessOptions, EntityType? entity, Access access) where EntityType : ShiftEntity<EntityType>, new();
