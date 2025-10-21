2b5aa8f26da15960d65c02e781f209a9019ba731
Saman <gtsaman@gmail.com>
2025-10-21 21:37:29 +0300

    Export: Update CustomColumnExportAttribute to use a formatter instead of joining each param

--- ShiftEntity.Model/ShiftListCustomColumnExportAttribute.cs
+++ ShiftEntity.Model/ShiftListCustomColumnExportAttribute.cs
@@ -1,39 +1,14 @@
 ﻿namespace ShiftSoftware.ShiftEntity.Model;
 
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-public class ShiftListCustomColumnExportAttribute : Attribute
+public class CustomColumnExportAttribute : Attribute
 {
-    public string[] Parts { get; }
+    public string Format { get; }
+    public string[] Args { get; }
 
-    public ShiftListCustomColumnExportAttribute(params string[] parts)
+    public CustomColumnExportAttribute(string format, params string[] args)
     {
-        Parts = parts;
-    }
-
-    public List<object> ToList()
-    {
-        var result = new List<object>();
-
-        foreach (var part in Parts)
-        {
-            if (part.StartsWith("Property."))
-            {
-                result.Add(new
-                {
-                    type = "property",
-                    value = part.Substring("Property.".Length)
-                });
-            }
-            else
-            {
-                result.Add(new
-                {
-                    type = "string",
-                    value = part
-                });
-            }
-        }
-
-        return result;
+        Args = args;
+        Format = format;
     }
 }
