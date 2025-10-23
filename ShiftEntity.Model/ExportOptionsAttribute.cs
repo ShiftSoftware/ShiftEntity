37923cbe088614d1d9e9cef6b44f06e8c16ffa6f
Saman <gtsaman@gmail.com>
2025-10-23 15:12:31 +0300

    Replace Export attributes with a single ExportOptionsAttribute and add hiding option

---
+++ ShiftEntity.Model/ExportOptionsAttribute.cs
@@ -0,0 +1,34 @@
+﻿namespace ShiftSoftware.ShiftEntity.Model;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+public class ExportOptionsAttribute : Attribute
+{
+    public bool Hidden { get; }
+    public string? Format { get; }
+    public string[]? Args { get; }
+
+    public ExportOptionsAttribute(bool hidden)
+    {
+        Hidden = hidden;
+    }
+
+    /// <summary>
+    /// NumberFormatter.
+    /// </summary>
+    /// <param name="format"></param>
+    public ExportOptionsAttribute(string format)
+    {
+        Format = format;
+    }
+
+    /// <summary>
+    /// CustomColumn Formatter.
+    /// </summary>
+    /// <param name="format"></param>
+    /// <param name="args"></param>
+    public ExportOptionsAttribute(string format, params string[] args)
+    {
+        Format = format;
+        Args = args;
+    }
+}
