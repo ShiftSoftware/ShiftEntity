d1d3b64939452236fd0fe2ccda382c12c507462a
AzaAsim <aza@shift.software>
2025-10-27 19:34:04 +0300

    Move Identity Database & Container Constants to ShiftEntity.Model

---
+++ ShiftEntity.Model/Replication/IdentityDatabaseAndContainerNames.cs
@@ -0,0 +1,13 @@
+﻿namespace ShiftSoftware.ShiftEntity.Model.Replication;
+
+public class IdentityDatabaseAndContainerNames
+{
+    public const string DatabaseName = "Identity";
+    public const string CompanyContainerName = "Companies";
+    public const string CompanyBranchContainerName = "CompanyBranches";
+    public const string CountryContainerName = "Countries";
+    public const string ServiceContainerName = "Services";
+    public const string DepartmentContainerName = "Departments";
+    public const string BrandContainerName = "Brands";
+    public const string TeamContainerName = "Teams";
+}
