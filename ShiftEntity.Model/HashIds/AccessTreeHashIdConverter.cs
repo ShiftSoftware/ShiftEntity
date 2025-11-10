1f23fd32910ccd9b8f535d96a8e660dde48e84dc
AzaAsim <aza@shift.software>
2025-11-10 14:10:36 +0300

    Add AccessTreeListDTO, use a dedicated IdHasher, and filter users by Access Tree in User List

---
+++ ShiftEntity.Model/HashIds/AccessTreeHashIdConverter.cs
@@ -0,0 +1,8 @@
+﻿namespace ShiftSoftware.ShiftEntity.Model.HashIds;
+
+public class AccessTreeHashIdConverter : JsonHashIdConverterAttribute<AccessTreeHashIdConverter>
+{
+    public AccessTreeHashIdConverter() : base(HashId.IdentityHashIdMinLength, HashId.IdentityHashIdSalt, HashId.IdentityHashIdAlphabet, true)
+    {
+    }
+}
