a0fb8bfd2307b3f769b39152199e5c2156cf636a
Aza Asim <azaasim@gmail.com>
2026-04-13 21:28:30 +0300

    Add User (Cosmos) Replication

---
+++ ShiftEntity.Model/Replication/IdentityModels/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using ShiftSoftware.ShiftEntity.Model.Flags;
+
+namespace ShiftSoftware.ShiftEntity.Model.Replication.IdentityModels;
+
+public class UserModel : 
+    ReplicationModel, 
+    IEntityHasCompany<UserModel>, 
+    IEntityHasCompanyBranch<UserModel>,
+    IEntityHasRegion<UserModel>,
+    IEntityHasCountry<UserModel>
+{
+    public string FullName { get; set; } = default!;
+    public string Username { get; set; } = default!;
+    public string? IntegrationId { get; set; }
+    public bool BuiltIn { get; set; }
+    public long? CompanyID { get; set; }
+    public long? CompanyBranchID { get; set; }
+    public long? RegionID { get; set; }
+    public long? CountryID { get; set; }
+}
