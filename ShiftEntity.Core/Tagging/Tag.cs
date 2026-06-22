65903a31d234577e0cb337cc6adb046c4ffbf683
Nahro Fuad <nahropro@gmail.com>
2026-06-23 02:51:25 +0300

    Add replication fields to Tag entity and migration

--- ShiftEntity.Core/Tagging/Tag.cs
+++ ShiftEntity.Core/Tagging/Tag.cs
@@ -1,8 +1,10 @@
+using ShiftSoftware.ShiftEntity.Model.Replication;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace ShiftSoftware.ShiftEntity.Core.Tagging;
 
-public class Tag : ShiftEntity<Tag>
+public class Tag : ShiftEntity<Tag>, IShiftEntityReplication
 {
     [Required]
     [MaxLength(128)]
@@ -16,4 +18,10 @@ public class Tag : ShiftEntity<Tag>
 
     [MaxLength(128)]
     public string? IntegrationID { get; set; }
+
+    // Replication bookkeeping columns (written only by the replication pipeline's MarkReplicated).
+    // Implementing IShiftEntityReplication lets a Tag be set up for Cosmos replication so tag edits
+    // can refresh their denormalized copies embedded in other documents.
+    public DateTimeOffset? LastReplicationDate { get; set; }
+    public string? LastReplicationStamp { get; set; }
 }
